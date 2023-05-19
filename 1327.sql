5448be24e46166b9a13e5016d490584b2b6783b8
GhazalADel <92995484+GhazalADel@users.noreply.github.com>
2023-05-19 19:38:08 +0330

    Create 1327.sql

---
+++ 1327.sql
@@ -0,0 +1,6 @@
+select p.product_name,sum(o.unit) 'unit'
+from Products p inner join Orders o
+on p.product_id = o.product_id
+where Left(order_date, 7) = '2020-02'
+group by p.product_name
+having sum(o.unit) >= 100 
